f6b2ec55017ffafcf24df06c6d0ba97b108ce842
Jim Cavera <j_cavera@yahoo.com>
2025-10-20 09:46:24 -0700

    Update lat_lon_encode.h

--- lat_lon_encode.h
+++ lat_lon_encode.h
@@ -1,31 +1,32 @@
 // Latitude/longitude encoding
+#include <math.h>
 
 double a     = 425983.999935;
 double b     = 67797.4592677;
 double torad = 0.01745329252;
 double xconv = 2366.57777742;
 
-inline unsigned long encode_lon_to_x (double longitude) {
+unsigned long encode_lon_to_x (double longitude) {
     return ((unsigned long) ((longitude + 180) * xconv));
 }
 
-inline unsigned long encode_lat_to_y (double latitiude) {
-    double S = sin(latitude * torad);
-    double Y = a – (b * log((1 + S) / (1 – S)));
+unsigned long encode_lat_to_y (double latitiude) {
+    double S = sin(latitiude * torad);
+    double Y = a - (b * log((1 + S) / (1 - S)));
     return ((unsigned long) Y);
 }
 
-double xcon  = 0.00042255108
-double twopi = 6.28318530718
-double bcon  = 1.47498152704E-5
-double todeg = 57.2957795131
+double xcon  = 0.00042255108;
+double twopi = 6.28318530718;
+double bcon  = 1.47498152704E-5;
+double todeg = 57.2957795131;
 
-inline double decode_x_to_lon (unsigned long X) {
-    return ((((double) X) * xcon) – 180.0);
+double decode_x_to_lon (unsigned long X) {
+    return ((((double) X) * xcon) - 180.0);
 }
 
-inline double decode_y_to_lat (unsigned long Y) {
-    double b = twopi – (Y * bcon);
+double decode_y_to_lat (unsigned long Y) {
+    double b = twopi - (Y * bcon);
     double a = pow(10, b);
-    return (todeg * (asin((a – 1)/(a + 1))));
+    return (todeg * (asin((a - 1)/(a + 1))));
 }
