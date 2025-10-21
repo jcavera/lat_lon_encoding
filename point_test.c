b6e3ca71c0304f3b20eb8493c68f19c888cf7880
Jim Cavera <j_cavera@yahoo.com>
2025-10-21 09:10:48 -0700

    Update point_test.c

--- point_test.c
+++ point_test.c
@@ -4,6 +4,7 @@
 #define PI         ((float) 3.14159265359)
 #define PI_2       ((float) 6.28318530718)    /* 2 * pi */
 #define PI_180     ((float) 0.01745329251)    /* pi / 180 */
+#define PI_360     ((float) 114.591559026)    /* 360 / pi */
 #define PI_4       ((float) 0.07957747154)    /* 1/(pi * 4) */
 #define mapSize    (0x00100000)               /* 256 << levelOfDetail [12] */
 
@@ -36,7 +37,7 @@ float PixelXToLon(int pixelX)
 float PixelYToLat(int pixelY)  
 {  
     float y = 0.5 - (Clip(pixelY, 0, mapSize - 1) / mapSize);  
-    return (90 - 360 * atan(exp(-y * PI_2)) / PI);  
+    return (90 - PI_360 * atan(exp(-y * PI_2)));
 }
 
 
@@ -54,10 +55,7 @@ int main(void)
     printf("lon i = %f \n", lon_i);
     printf("x     = %08x \n", x);
     printf("y     = %08x \n", y);
-    printf("lat f = %f \n", lat_f);
-    printf("lon f = %f \n", lon_f);
-    printf("err x = %f \n", (lon_f - lon_i));
-    printf("err y = %f \n", (lat_f - lat_i));
-    
+    printf("lat f = %f [ err y = %f ]\n", lat_f, (lat_f - lat_i));
+    printf("lon f = %f [ err x = %f ]\n", lon_f, (lon_f - lon_i));
     return 0;
 }
