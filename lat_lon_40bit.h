71c5595161253556d7bea89ef0170b941997e768
Jim Cavera <j_cavera@yahoo.com>
2025-10-29 10:32:40 -0700

    Update lat_lon_40bit.h

--- lat_lon_40bit.h
+++ lat_lon_40bit.h
@@ -1,12 +1,15 @@
-int   encode_lon_to_x  (FTYPE longitude);
-int   encode_lat_to_y  (FTYPE latitude);
-FTYPE decode_x_to_lon  (int pixelX);
-FTYPE decode_y_to_lat  (int pixelY);
+#define LL40_FTYPE  double               /* floating point data type */
+#define LL40_ITYPE  unsigned long long   /* 64b unsigned integer type */
 
-ITYPE combine_xy       (int x, int y);
-int   split_x_from_xy  (ITYPE xy);
-int   split_y_from_xy  (ITYPE xy);
+int        encode_lon_to_x  (LL40_FTYPE longitude);
+int        encode_lat_to_y  (LL40_FTYPE latitude);
+LL40_FTYPE decode_x_to_lon  (int pixelX);
+LL40_FTYPE decode_y_to_lat  (int pixelY);
 
-ITYPE encode_to_xy     (FTYPE lat, FTYPE lon);
-FTYPE decode_xy_to_lat (ITYPE xy);
-FTYPE decode_xy_to_lon (ITYPE xy);
+LL40_ITYPE combine_xy       (int x, int y);
+int        split_x_from_xy  (LL40_ITYPE xy);
+int        split_y_from_xy  (LL40_ITYPE xy);
+
+LL40_ITYPE encode_to_xy     (LL40_FTYPE lat, FTYPE lon);
+LL40_FTYPE decode_xy_to_lat (LL40_ITYPE xy);
+LL40_FTYPE decode_xy_to_lon (LL40_ITYPE xy);
